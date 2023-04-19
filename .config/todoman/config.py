bb436340410ed3e9b526d5a0eb85d1b464a05c99
Dmitriy Krasilnikov <krasilnikovdo@gmail.com>
2023-04-19 22:20:56 +0200

    add todoman config

---
+++ .config/todoman/config.py
@@ -0,0 +1,6 @@
+# A glob expression which matches all directories relevant.
+path = "~/Dropbox/todoman/*"
+date_format = "%Y-%m-%d"
+time_format = "%H:%M"
+default_list = "Personal"
+default_due = 48
