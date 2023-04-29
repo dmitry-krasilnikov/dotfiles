215128f03b99f13c99bd690d2ec7f87af03e031b
Dmitriy Krasilnikov <krasilnikovdo@gmail.com>
2023-04-29 16:02:52 +0200

    add simple script to install deps

---
+++ .bin/dotfiles_install.sh
@@ -0,0 +1,28 @@
+#!/bin/env zsh
+
+paru -S xorg-apps \
+        xcape \
+        redshift \
+        autorandr \
+        zsh-completions \
+        xss-lock \
+        htop \
+        strace \
+        python-wal \
+        lazygit \
+        pulsemixer \
+        scrot \
+        feh \
+        unclutter \
+        polybar \
+        i3lock \
+        powerline-fonts \
+        rofi \
+        ttf-font-awesome \
+        i3-wm \
+        # i3status-rust \  # TODO: try it out some time
+        ttf-hack-nerd \
+        todoman \
+        python-click-repl \  # optional for todoman
+        alacritty \
+        wezterm
