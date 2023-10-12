d888aaca318ee446706014dda3bcc5601949cfde
Dmitriy Krasilnikov <krasilnikov.d.o@gmail.com>
2023-10-12 23:48:46 +0200

    add tmux command to switch between projects

---
+++ .bin/tmux/projects.sh
@@ -0,0 +1,1 @@
+fd --unrestricted "\.git$" --search-path ~ -x echo "{//}" | rg -v "/\." | awk '{print $0, gensub(".*/", "", "g", $0)}' | fzf --delimiter=" " --with-nth=1 --bind "enter:execute-silent(tmux new-session -c {1} -s {2} -d; tmux switch-client -t {2})+abort"
