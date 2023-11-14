509f6d189dcce73fe8e6169521a153e338e0f24e
Dmitriy Krasilnikov <krasilnikov.d.o@gmail.com>
2023-11-14 17:59:30 +0100

    refine tmux projects command

--- .bin/tmux/projects.sh
+++ .bin/tmux/projects.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 fd --unrestricted "\.git$" --search-path ~ -x echo "{//}" | rg -v "/\." | awk '{print $0, gensub(".*/", "", "g", $0)}' | fzf --delimiter=" " --with-nth=1 --bind "enter:execute-silent(tmux new-session -c {1} -s {2} -d; tmux switch-client -t {2})+abort"
