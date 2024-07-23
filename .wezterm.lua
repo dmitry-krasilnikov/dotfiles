34c6435fb581ac347e274c915551726a6a7b1e6b
Dmitriy Krasilnikov <krasilnikov.d.o@gmail.com>
2024-07-23 09:25:50 +0200

    improve wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,15 +1,16 @@
 local wezterm = require 'wezterm'
 local config = {}
 
--- config.color_scheme = 'Catppuccin Latte'
 config.colors = require('lua/colorschemes/rose-pine-dawn').colors()
 
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+	left = 2,
+	right = 2,
+	top = 2,
+	bottom = 2,
 }
 config.font = wezterm.font 'Hack Nerd Font'
 
